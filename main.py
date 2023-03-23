4612d918da2466c7d56d5e498fc992b429b77ffa
Konan03 <manuelcaicedo52@gmail.com>
2023-03-23 11:07:26 -0500

    archivo main con data para poder iniciar el lidar

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("lidar")
+import sicktoolbox as sbt
+lms = SickLMS()
+
+#lidar = sbt.SickLMS('192.168.1.1') # Reemplaza la dirección IP con la dirección IP de tu sensor LiDAR SICK
+if lms.Connect(port="/dev/ttyUSB0", baudrate=38400):
+    print("Connected to LiDAR")
+else:
+    print("Connection failed")
+
+#lidar.Connect()
+while True:
+    scan_data = lms.GetSickScanData()
+    print(scan_data)
+
+lms.Disconnect()
